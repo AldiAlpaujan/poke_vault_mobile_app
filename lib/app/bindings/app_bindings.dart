a18aee9814448370edef442d064bcc1b656c70e1
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2025-12-04 10:32:32 +0700

    minify DI appconfig

--- lib/app/bindings/app_bindings.dart
+++ lib/app/bindings/app_bindings.dart
@@ -3,17 +3,8 @@ import 'package:flutter_project_template/app/bindings/feature_bindings.dart';
 import 'package:get/get_instance/get_instance.dart';
 
 class AppBindings extends Bindings {
-  static final appConfigDependencies = <Function>[];
-
-  static void registerAppConfig(Function dependency) {
-    appConfigDependencies.add(dependency);
-  }
-
   @override
   void dependencies() {
-    for (Function dependency in appConfigDependencies) {
-      dependency();
-    }
     CoreBindings().dependencies();
     FeatureBindings().dependencies();
   }
--- lib/main/main_development.dart
+++ lib/main/main_development.dart
@@ -1,17 +1,14 @@
-import 'package:flutter_project_template/app/bindings/app_bindings.dart';
 import 'package:flutter_project_template/bootstrap.dart';
 import 'package:flutter_project_template/config/app_config.dart';
 import 'package:flutter_project_template/shared/enums/app_flavor.dart';
 import 'package:get/instance_manager.dart';
 
 void main() {
-  AppBindings.registerAppConfig(
-    () => Get.put(
-      AppConfig(
-        flavor: AppFlavor.development,
-        appName: "Project Template",
-        apiUrl: "https://aldimalpaujanbackend-production.up.railway.app/api",
-      ),
+  Get.put(
+    AppConfig(
+      flavor: AppFlavor.development,
+      appName: "Project Template",
+      apiUrl: "https://aldimalpaujanbackend-production.up.railway.app/api",
     ),
   );
   bootstrap();
